f0bf335d79d48e2721da577838749747e325e782
mrbassx <abassiddrisu9222@gmail.com>
2025-06-17 11:39:56 +0000

    updated...

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
