56545345978dc4b22ee358f6210788c2a9e6d40c
mrbassx <abassiddrisu9222@gmail.com>
2025-06-26 08:43:36 +0000

    up

--- models/Cart.js
+++ models/Cart.js
@@ -1,10 +1,10 @@
 const  mongoose = require("mongoose");
-const User = require("./User")
+// const User = require("./User")
 
 
 const cartSchema = mongoose.Schema({
     quantity:String,
-    user:User
+    
 });
 
 
