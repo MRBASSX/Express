c3790d96647d20cea81bacd8679c335bbbdb9026
mrbassx <abassiddrisu9222@gmail.com>
2025-06-30 09:11:52 +0000

    up

--- app.js
+++ app.js
@@ -25,8 +25,13 @@ app.use('/', indexRouter);
 
 
 
+
 // Replace "mydatabase" with your actual database name
- res.redirect("/");
+mongoose.connect('mongodb://localhost:27017/mydatabase').then(() => {
+  console.log('✅ Connected to MongoDB');
+}).catch((err) => {
+  console.error('❌ MongoDB connection error:', err);
+});
 
 
 
