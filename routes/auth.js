572c757edad0771eb6d10be12d3db5ada341d6e8
mrbassx <abassiddrisu9222@gmail.com>
2025-07-03 08:49:31 +0000

    updated..

---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { register, login } = require('../controllers/authController');
+const authenticateJWT = require('../middleware/authMiddleware');
+
+router.post('/register', register);
+router.post('/login', login);
+
+//Protected route
+router.get('/profile', authenticateJWT, (req, res) => {
+    
+    res.json({ message: 'This is a protected route', user: req.user });
+});
+
+module.exports = router;
