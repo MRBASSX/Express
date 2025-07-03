4892653647e7cb797573114eb04da9625bf9b7fd
mrbassx <abassiddrisu9222@gmail.com>
2025-07-03 08:43:21 +0000

    updated..

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/Router');
+require('dotenv').config();
 
 const mongo= require('mongoose');
 var app = express();
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,34 @@
-const User = require("../models/userModel");
-
-const createUser = async (req,res)=>{
-
-    const user =  new User(req.body);
-    const saved = await user.save();
-
-   
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const users = require('../models/user');
 
 
-}
+const register = async (req, res) => {
+    const { username, password } = req.body;
 
+    const existingUser = users.find(u => u.username === username);
+    if (existingUser) return res.status(400).json({ message: 'User already exists' });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    users.push({ username, password: hashedPassword });
+
+    res.status(201).json({ message: 'User registered successfully' });
+};
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = users.find(u => u.username === username);
+    if (!user) return res.status(401).json({ message: 'Invalid credentials' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+
+    res.json({ token });
+};
+
+module.exports = { register, login };
